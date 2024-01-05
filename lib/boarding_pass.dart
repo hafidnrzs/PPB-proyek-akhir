6f640dfb02a45e80a0f9518aad90d609668f0840
hafidnrzs <hafidnurazis@gmail.com>
2024-01-06 02:26:31 +0700

    Buat kerangka boarding pass page
    - Revisi sedikit color scheme dan theme

--- lib/boarding_pass.dart
+++ lib/boarding_pass.dart
@@ -1,13 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:ppb_proyek_akhir/color_schemes.g.dart';
 
 class BoardingPass extends StatelessWidget {
+  const BoardingPass({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(),
+        leading: const BackButton(),
         title: const Text('Boarding Pass'),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: const DefaultTextStyle(
+                style: TextStyle(color: Colors.black),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text('Logo'),
+                    Divider(
+                      color: Colors.black,
+                      height: 1,
+                      thickness: 1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Earth'),
+                            Text('ETH'),
+                            Text('05 January 2023'),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('Earth'),
+                            Text('ETH'),
+                            Text('05 January 2023'),
+                          ],
+                        )
+                      ],
+                    ),
+                    Text('Kode Booking'),
+                    Text('7BG55A'),
+                    Text('Nama Penumpang'),
+                    Text('John Doe'),
+                    Text('No Kursi'),
+                    Text('11A'),
+                    Divider(
+                      color: Colors.black,
+                      height: 1,
+                      thickness: 1,
+                    ),
+                    Text('Barcode'),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 32),
+            FilledButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                backgroundColor: darkColorScheme.background,
+                foregroundColor: darkColorScheme.inverseSurface,
+              ),
+              child: const Text(
+                'Download',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MainApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       title: 'Astral Express Ticket App',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
