d067c2a62defdecf6873f09770b18af0bca606e6
hafidnrzs <hafidnurazis@gmail.com>
2024-01-07 00:13:21 +0700

    Update boarding pass page

--- lib/boarding_pass.dart
+++ lib/boarding_pass.dart
@@ -1,93 +1,200 @@
 import 'package:flutter/material.dart';
 import 'package:ppb_proyek_akhir/color_schemes.g.dart';
+import 'package:syncfusion_flutter_barcodes/barcodes.dart';
 
-class BoardingPass extends StatelessWidget {
+class BoardingPass extends StatefulWidget {
   const BoardingPass({super.key});
+
+  @override
+  State<BoardingPass> createState() => _BoardingPassState();
+}
+
+class _BoardingPassState extends State<BoardingPass> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: darkColorScheme.onPrimary,
       appBar: AppBar(
         leading: const BackButton(),
         title: const Text('Boarding Pass'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(24),
-              ),
-              padding: const EdgeInsets.all(16),
-              child: const DefaultTextStyle(
-                style: TextStyle(color: Colors.black),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text('Logo'),
-                    Divider(
-                      color: Colors.black,
-                      height: 1,
-                      thickness: 1,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                padding: const EdgeInsets.all(24),
+                child: DefaultTextStyle(
+                  style: const TextStyle(color: Colors.black87),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Image(
+                        image: AssetImage('assets/HSR_logo.png'),
+                        height: 60,
+                        alignment: Alignment.topLeft,
+                      ),
+                      const SizedBox(height: 8),
+                      const Divider(
+                        color: Colors.grey,
+                        height: 1,
+                        thickness: 1,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text('Earth'),
-                            Text('ETH'),
-                            Text('05 January 2023'),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Earth'),
+                                Text(
+                                  'ETH',
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Text('05 January 2023'),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text('Jarilo VI'),
+                                Text(
+                                  'JRL',
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Text('08 January 2023'),
+                              ],
+                            )
                           ],
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        height: 1,
+                        thickness: 1,
+                      ),
+                      const SizedBox(height: 16),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: [
-                            Text('Earth'),
-                            Text('ETH'),
-                            Text('05 January 2023'),
+                            Text('Kode Booking'),
+                            SizedBox(height: 8),
+                            Text(
+                              '7BG55A',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
                           ],
-                        )
-                      ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text('Nama Penumpang'),
+                            SizedBox(height: 8),
+                            Text(
+                              'JOHN DOE',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text('No Kursi'),
+                            SizedBox(height: 8),
+                            Text(
+                              '11A',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      const Divider(
+                        color: Colors.grey,
+                        height: 1,
+                        thickness: 1,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 20),
+                        height: 100,
+                        child: SfBarcodeGenerator(
+                          value: '7BG55A',
+                          barColor: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              FilledButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  backgroundColor: darkColorScheme.background,
+                  foregroundColor: darkColorScheme.inverseSurface,
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.download),
+                    SizedBox(width: 8),
+                    Text(
+                      'Download Tiket',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    Text('Kode Booking'),
-                    Text('7BG55A'),
-                    Text('Nama Penumpang'),
-                    Text('John Doe'),
-                    Text('No Kursi'),
-                    Text('11A'),
-                    Divider(
-                      color: Colors.black,
-                      height: 1,
-                      thickness: 1,
-                    ),
-                    Text('Barcode'),
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 32),
-            FilledButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                backgroundColor: darkColorScheme.background,
-                foregroundColor: darkColorScheme.inverseSurface,
-              ),
-              child: const Text(
-                'Download',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
